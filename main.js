aae7244d9dc1f760c15ec4ea3172233410a48144
Margarida <margarida97pt@gmail.com>
2020-10-27 17:16:26 +0000

    working on it

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // main.js
-
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 const timer = document.getElementById('timer');
+let fiveSeconds = 5;
+let intervalId;
+
 
 // GRID Calculations
 
@@ -12,6 +14,10 @@ const tileCount = 6;
 const tileSize = width / tileCount;
 const squares = [];
 const colors = ['#66cdaa', '#ff6b6b','#e27fa0','#ddf5f9','#1f4c25', '#ff7e00','#ffa3dd', '#99f4f2', '#b4a097', '#eaea32', '	#15b4a3', '	#ff0080'];
+let firstRandomSquare;
+let score = 0;
+
+
 // Iteration 1
 
 function drawGrid() {
@@ -24,45 +30,75 @@ function drawGrid() {
 
         context.fillStyle = randomColor;
         context.fillRect(j * tileSize, i * tileSize, tileSize, tileSize, colors);
-        console.log('drawing position x ', j * tileSize, 'position y', i * tileSize,);
+     //   console.log('drawing position x ', j * tileSize, 'position y', i * tileSize,);
         squares.push({ x: j * tileSize, y:  i * tileSize}); //inserimos 36 quadrados ({x: y:})
     }
   }
 }
 
 function paintTwoRandomSquares() {
-  //  for (let i = 0; i < tileCount; i++) {
-    //    for (let j = 0; j < tileCount; j++) {
             let randomIndex = Math.floor(Math.random() * squares.length);
-            let firstRandomSquare = squares[randomIndex];
-            console.log(firstRandomSquare);
+            firstRandomSquare = squares[randomIndex];
+       //     console.log('firstRandomSquare: => ', firstRandomSquare);
+            context.fillStyle = 'black';
+            context.fillRect(firstRandomSquare.x, firstRandomSquare.y, tileSize, tileSize);
             //Obter x e y de firstRandomSquare e fazer fillRect(x, y, width, height)
-
-            
-         //   if (randomIndex === 36) {
-           //     getTwoRandomSquares = 2;1
 }
 
-      //2 indexes aleatoriamente
-      //ir buscar 
-  
+
+function getCursorPosition(canvas, event) {
+  const rect = canvas.getBoundingClientRect()
+  const clickedX = event.clientX - rect.left
+  const clickedY = event.clientY - rect.top
+ // console.log('black square coordinates', firstRandomSquare.x, firstRandomSquare.y);
+ // console.log("x: " + x + " y: " + y)
+
+if (!((clickedY > firstRandomSquare.y + tileSize) || 
+  (clickedY < firstRandomSquare.y) ||
+  (clickedX > firstRandomSquare.x + tileSize) ||
+  (clickedX < firstRandomSquare.x)))
+{
+  score += 1;
+  document.getElementById('scoreTotal').innerText = score;
+ 
+
+}
+}
+
+
+canvas.addEventListener('mousedown', function(e) {
+  getCursorPosition(canvas, e)
+})
+
+
 
 // Draw Everything
-
-function drawEverything() {
+function drawEverything() {  
     context.clearRect(0, 0, width, height);
     drawGrid();
     setTimeout(()=> {
         paintTwoRandomSquares();
-    },1000);
+    },200);
   }
 
   // Triggering the first drawEverything after 500ms ensures that all pictures had been loaded
-  setTimeout(drawEverything, 500);
+drawEverything();
+
+function startGame() {
+  intervalId = setInterval(()=> {
+    drawEverything(); //VAI SER EXECUTADO DE 500 MILISEGUNDOS EM 500 MILISEGUNDOS
+  }, 700)
+}
+
+startGame();
+
+ 
+
+//function restartGame(restart) {
+ // if(decreaseTime===0){
+   // restart1.style
 
 //TIME
-
-let fiveSeconds = 5;
 
  function decreaseTime(fiveSeconds) {
 
@@ -70,8 +106,13 @@ let fiveSeconds = 5;
         fiveSeconds--;
 
         if (fiveSeconds <= 0) {
-            clearInterval(interval)
+            clearInterval(interval);
+            clearInterval(intervalId);
             fiveSeconds = `Times's up!`
+            getCursorPosition = stop;
+            let restartButton = document.getElementById('restart');
+            restartButton.style.display = 'block';
+        
         }
         timer.innerHTML = fiveSeconds
     }, 1000)
@@ -79,29 +120,11 @@ let fiveSeconds = 5;
 
  decreaseTime(fiveSeconds);
 
-/*   adding this JS code gives you an on click event that'll let you click and paint an area the size of one of your squares red, you just need to use the fill style as a letiable that the user can change.
-
-function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-          x: evt.clientX - rect.left,
-          y: evt.clientY - rect.top
-        };
-      }
-function getNearestSquare(position) {
- var x = position.x;
-    var y = position.y;
-if (x < 0 || y < 0) return null;
-    x = (Math.floor(x / 20) * 20) + 0.5
-    y = (Math.floor(y / 20) * 20) + 0.5
-    return {x: x, y: y};
-}
-$(c_canvas).click(function(evt) {
-    var pos = getNearestSquare(getMousePos(c_canvas, evt));
-    if (pos != null) {
-        context.fillStyle="#FF0000";
-        context.fillRect(pos.x,pos.y,20,20);
-    }
-});
-
-*/
+ document.getElementById('restart').onclick = () => {
+   console.log('restarting')
+   fiveSeconds = 5;
+   drawEverything();
+   decreaseTime(fiveSeconds);
+   startGame();
+   document.getElementById('scoreTotal') = 0;
+ };
